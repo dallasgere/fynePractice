421980952d97bcbc03fc8b1dbe072adecd66b13e
dallas <dallasgere@icloud.com>
2023-10-12 22:37:49 -0500

    adding some readme stuff

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # fynePractice
+
+## What is this
+* this is a tutorial that I followed from 
+    * https://dev.to/vikkio88/creating-a-todo-app-in-fyne-an-go-2i6a
+
+## What am I using this for
+* I wanted this to help me learn fyne and get better at it
+* I will try and add some customizations to this tomorrow
--- main.go
+++ main.go
@@ -21,9 +21,11 @@ func main() {
 		models.NewTodo("Some other things"),
 	}
 	todos := binding.NewUntypedList()
+
 	for _, t := range data {
 		todos.Append(t)
 	}
+
 	newtodoDescTxt := widget.NewEntry()
 	newtodoDescTxt.PlaceHolder = "New Todo Description..."
 	addBtn := widget.NewButton("Add", func() {
@@ -31,6 +33,7 @@ func main() {
 		newtodoDescTxt.Text = ""
 	})
 	addBtn.Disable()
+
 	newtodoDescTxt.OnChanged = func(s string) {
 		addBtn.Disable()
 
@@ -53,33 +56,27 @@ func main() {
 			),
 			nil, // Right
 			nil, // Left
-			// the rest will take all the rest of the space
+
 			widget.NewListWithData(
-				// the binding.List type
+
 				todos,
-				// func that returns the component structure of the List Item
-				// exactly the same as the Simple List
+
 				func() fyne.CanvasObject {
 					return container.NewBorder(
 						nil, nil, nil,
-						// left of the border
+
 						widget.NewCheck("", func(b bool) {}),
-						// takes the rest of the space
+
 						widget.NewLabel(""),
 					)
 				},
-				// func that is called for each item in the list and allows
-				// but this time we get the actual DataItem we need to cast
+
 				func(di binding.DataItem, o fyne.CanvasObject) {
 					ctr, _ := o.(*fyne.Container)
-					// ideally we should check `ok` for each one of those casting
-					// but we know that they are those types for sure
+
 					l := ctr.Objects[0].(*widget.Label)
 					c := ctr.Objects[1].(*widget.Check)
-					/*
-						diu, _ := di.(binding.Untyped).Get()
-						todo := diu.(models.Todo)
-					*/
+
 					todo := models.NewTodoFromDataItem(di)
 					l.SetText(todo.Description)
 					c.SetChecked(todo.Done)
